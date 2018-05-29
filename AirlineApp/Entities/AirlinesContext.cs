f97e6154f2ad2d61c87aae3d4ff082ab0720f401
svend <sven.depickere@student.howest.be>
2018-05-29 13:22:18 +0200

    Second Change the airlinecontext

--- AirlineApp/Entities/AirlinesContext.cs
+++ AirlineApp/Entities/AirlinesContext.cs
@@ -10,14 +10,19 @@ namespace AirlineApp.Entities
         public virtual DbSet<Locatie> Locatie { get; set; }
         public virtual DbSet<Opgericht> Opgericht { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public AirlinesContext(DbContextOptions<AirlinesContext> options) : base(options)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-UUQ7FUO\SQLEXPRESS;Initial Catalog=Airlines;Integrated Security=True;");
-            }
+
         }
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-UUQ7FUO\SQLEXPRESS;Initial Catalog=Airlines;Integrated Security=True;");
+        //            }
+        //        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
