e3a77dd10292f7413bcd93674aeb195a568f5b39
svend <sven.depickere@student.howest.be>
2018-06-05 11:23:22 +0200

    Added API list controller

---
+++ AirlineApp/Controllers/ListAPIController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineApp.Controllers
+{
+    public class ListAPIController : Controller
+    {
+        // GET: ListAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
