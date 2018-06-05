68d72869627165d1d27ae48845e4c9b60314f22e
svend <sven.depickere@student.howest.be>
2018-06-05 10:29:58 +0200

    Update Airlinecodes with the new entitie

--- AirlineApp/Entities/Airlinecodes.cs
+++ AirlineApp/Entities/Airlinecodes.cs
@@ -5,10 +5,16 @@ namespace AirlineApp.Entities
 {
     public partial class Airlinecodes
     {
+        public Airlinecodes()
+        {
+            FlightData = new HashSet<FlightData>();
+        }
+
         public string Name { get; set; }
         public string Code { get; set; }
 
         public Locatie Locatie { get; set; }
         public Opgericht Opgericht { get; set; }
+        public ICollection<FlightData> FlightData { get; set; }
     }
 }
