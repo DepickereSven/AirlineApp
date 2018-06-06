ee1eac2f30816d48e2612f15ff58c4a379904820
svend <sven.depickere@student.howest.be>
2018-06-06 08:14:09 +0200

    removed models and maked nested objects

--- AirlineApp/Controllers/FlightDataController.cs
+++ AirlineApp/Controllers/FlightDataController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using AirlineApp.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using static AirlineApp.Models.Airliner;
 
 namespace AirlineApp.Controllers
 {
@@ -25,13 +24,25 @@ namespace AirlineApp.Controllers
         {
             if (id == "all")
             {
-                var item = db.Airlinecodes.Select(x => new Airlines { Code = x.Code, Name = x.Name }).ToArray();
+                var item = db.Airlinecodes
+                    .Select(x => new
+                    {
+                        Code = x.Code,
+                        Name = x.Name
+                    })
+                    .ToArray();
                 return Ok(item);
 
-            } else
+            }
+            else
             {
-                var item = db.Airlinecodes.Find(id);
-                var newItem = (new Airlines { Code = item.Code, Name = item.Name });
+                var item = db.Airlinecodes
+                    .Find(id);
+                var newItem = (new
+                {
+                    Code = item.Code,
+                    Name = item.Name
+                });
                 if (item == null)
                 {
                     return Ok($"Airline with like {id} don't exist");
@@ -40,8 +51,37 @@ namespace AirlineApp.Controllers
             }
         }
 
-        [HttpGet("")]
-        
-        
+        [HttpGet("date/{dateStamp}")]
+        public IActionResult GetStamp(string dateStamp)
+        {
+            var item = db.FlightData
+                .Where(x => x.Date == DateTime.Parse(dateStamp))
+                .Select(x => new
+                {
+                    date = x.Date,
+                    AirlineCode = x.AirlineCode,
+                    Departure = new
+                    {
+                        Airport = x.DepatureAirport,
+                        State = x.DepatureState,
+                        Latitude = x.DepartureLatitude,
+                        Longitude = x.DepatureLongitude
+                    },
+                    Arrival = new
+                    {
+                        Airport = x.ArrivalAirport,
+                        State = x.ArrivalState,
+                        Latitude = x.ArrivalLatitude,
+                        Longitude = x.ArrivalLongitude
+                    }
+                });
+            if (item == null)
+            {
+                return Ok($"The date you give enterd: {dateStamp} isn't between: 2011-01-01 - 2011-01-03");
+            }
+            return Ok(item);
+        }
+
+
     }
 }
