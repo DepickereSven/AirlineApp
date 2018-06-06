2e35cc6805c5efed86c62a8516e8128132324f78
svend <sven.depickere@student.howest.be>
2018-06-06 09:52:13 +0200

    Added state/{depatureOrArrival}/{stateName} API

--- AirlineApp/Controllers/FlightDataController.cs
+++ AirlineApp/Controllers/FlightDataController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using AirlineApp.Entities;
@@ -33,7 +34,6 @@ namespace AirlineApp.Controllers
                     })
                     .ToArray();
                 return Ok(item);
-
             }
             else
             {
@@ -59,7 +59,7 @@ namespace AirlineApp.Controllers
                 .Where(x => x.Date == DateTime.Parse(dateStamp))
                 .Select(x => new
                 {
-                    date = x.Date,
+                    date = x.Date.ToString("d", CultureInfo.CreateSpecificCulture("ja-JP")),
                     AirlineCode = x.AirlineCode,
                     Departure = new
                     {
@@ -84,5 +84,74 @@ namespace AirlineApp.Controllers
         }
 
 
+        [HttpGet("state/{depatureOrArrival}/{stateName}")]
+        public IActionResult GetStates(string depatureOrArrival, string stateName)
+        {
+            if (depatureOrArrival == "DS" | depatureOrArrival == "ds")
+            {
+                var item = db.FlightData
+                    .Where(x => x.DepatureState == stateName)
+                    .Select(x => new
+                    {
+                        date = x.Date.ToString("d", CultureInfo.CreateSpecificCulture("ja-JP")),
+                        AirlineCode = x.AirlineCode,
+                        Departure = new
+                        {
+                            Airport = x.DepatureAirport,
+                            State = x.DepatureState,
+                            Latitude = x.DepartureLatitude,
+                            Longitude = x.DepatureLongitude
+                        },
+                        Arrival = new
+                        {
+                            Airport = x.ArrivalAirport,
+                            State = x.ArrivalState,
+                            Latitude = x.ArrivalLatitude,
+                            Longitude = x.ArrivalLongitude
+                        }
+                    });
+                if (item.Count() == 0)
+                {
+                    return Ok($"The state for depature: {stateName} you gave up don't have any records");
+                }
+                return Ok(item);
+            }
+            else
+            {
+                if (depatureOrArrival == "AS" | depatureOrArrival == "as")
+                {
+                    var item = db.FlightData
+                    .Where(x => x.ArrivalState == stateName)
+                    .Select(x => new
+                    {
+                        date = x.Date.ToString("d", CultureInfo.CreateSpecificCulture("ja-JP")),
+                        AirlineCode = x.AirlineCode,
+                        Departure = new
+                        {
+                            Airport = x.DepatureAirport,
+                            State = x.DepatureState,
+                            Latitude = x.DepartureLatitude,
+                            Longitude = x.DepatureLongitude
+                        },
+                        Arrival = new
+                        {
+                            Airport = x.ArrivalAirport,
+                            State = x.ArrivalState,
+                            Latitude = x.ArrivalLatitude,
+                            Longitude = x.ArrivalLongitude
+                        }
+                    });
+                    if (item.Count() == 0)
+                    {
+                        return Ok($"The state for arrival: {stateName} you gave up don't have any records");
+                    }
+                    return Ok(item);
+                }
+                else
+                {
+                    return Ok($"You entered {depatureOrArrival} that isn't equal to DS or AS");
+                }
+            }
+        }
     }
 }
